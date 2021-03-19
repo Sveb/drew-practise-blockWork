c438d646eecf4e4cc7a587cc41d82276ecd5ab7f
Sveb <sveborsoric@gmail.com>
2021-03-19 17:36:12 +0100

    Created
    Created and finished today

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <blockwork
+    title="Svebor"
+    text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+    image="../assets/images/ballons.jpeg"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import blockwork from "./components/blockwork.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    blockwork
   }
-}
+};
 </script>
 
 <style>
@@ -19,7 +22,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/blockwork.vue
@@ -0,0 +1,164 @@
+<template lang="html">
+  <div class="container">
+    <div class="card">
+      <div class="image-container">
+        <img src="../assets/images/ballons.jpeg" alt="" />
+      </div>
+
+      <div class="text-container">
+        <h1>{{ title }}</h1>
+        <p>{{ text }}</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image-container">
+        <img src="../assets/images/jojo.jpg" alt="" />
+      </div>
+      <div class="text-container">
+        <h1>{{ title }}</h1>
+        <p>{{ text }}</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image-container">
+        <img src="../assets/images/city.jpeg" alt="" />
+      </div>
+      <div class="text-container">
+        <h1>{{ title }}</h1>
+        <p>{{ text }}</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image-container">
+        <img src="../assets/images/angel.jpg" alt="" />
+      </div>
+      <div class="text-container">
+        <h1>{{ title }}</h1>
+        <p>{{ text }}</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image-container">
+        <img src="../assets/images/jojo2.png" alt="" />
+      </div>
+      <div class="text-container">
+        <h1>{{ title }}</h1>
+        <p>{{ text }}</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image-container">
+        <img src="../assets/images/jojo3.jpg" alt="" />
+      </div>
+      <div class="text-container">
+        <h1>{{ title }}</h1>
+        <p>{{ text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      // Mock: api.page
+      type: Object,
+      default: () => {}
+    },
+    isFullWidth: {
+      type: Boolean,
+      default: true
+    },
+    to: {
+      type: String,
+      default: ""
+    },
+    title: {
+      type: String,
+      default: ""
+    },
+    text: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+/* // fonts // */
+
+@font-face {
+  font-family: "cirka";
+  src: url(../assets/fonts/cirka-regular.woff);
+}
+@font-face {
+  font-family: "founders";
+  src: url(../assets/fonts/founders-grotesk-light.woff);
+}
+
+.container {
+  background-color: #fccc2e;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.card {
+  height: auto;
+  width: 44%;
+  margin: 49px 60px 150px 60px;
+  box-sizing: content-box;
+  overflow: hidden;
+}
+
+.image-container {
+  transition: 0.3s ease-out;
+}
+
+.image-container img {
+  width: 100%;
+  height: auto;
+}
+
+.card:nth-of-type(5) {
+  width: 95%;
+  margin: auto;
+}
+
+.text-container {
+  margin-top: 0;
+  margin-left: 20px;
+  padding: 10px;
+}
+
+.text-container h1 {
+  font-family: cirka;
+  margin-top: 0;
+  margin-bottom: 15px;
+  font-size: 50px;
+  padding: 0;
+}
+
+.text-container p {
+  font-size: 26px;
+  font-family: founders;
+  margin-top: 0;
+}
+
+/* HOVER STATE */
+
+.card:hover .image-container {
+  transform: scale(1.05);
+}
+
+/* BREAK POINT */
+
+@media (max-width: 1364px) {
+  .container {
+    flex-direction: column;
+  }
+}
+</style>
